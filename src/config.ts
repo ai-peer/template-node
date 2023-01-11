74d5888083342d94654b7e01efec2b6fe286632f
sd <leowest1921@gmail.com>
2023-01-11 20:23:36 +0800

    add

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-const isDev = process.env.NODE_ENV == "development";
-
-export default {
-   isDev: isDev,
-   logger: isDev ? "debug" : "info",
-   loggerDay: isDev ? "1d" : "30d",
-   loggerSize: "20m",
-};
