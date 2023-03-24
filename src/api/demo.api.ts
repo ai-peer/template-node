21cab607607b8a78e034fbf0d8d26ffac77d35fc
sd <leowest1921@gmail.com>
2023-03-24 22:48:31 +0800

    add

--- src/api/demo.api.ts
+++ src/api/demo.api.ts
@@ -1,15 +1,13 @@
 import { tagsAll, summary, query, body } from "koa-swagger-decorator";
 import BaseApi, { router, Context } from "../core/api";
-import * as utils from "../utils";
+
 @tagsAll(["前端/节点管理"])
 export default class AppMedisApi extends BaseApi {
    @router("get", "/ip")
    @router("post", "/ip")
    @summary("ip")
    async get(ctx: Context) {
-      let ip = this.getClientIp()
-         .replace(/[; ,].*/, "")
-         .trim();
+      let ip = this.getClientIp();
       let contentType = ctx.get("content-type");
       if (/json/i.test(contentType)) {
          ctx.body = JSON.stringify({
