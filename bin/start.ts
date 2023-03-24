5f8e55f1fd6ce949d8cbdde7b9458d87da0b2dd5
sd <leowest1921@gmail.com>
2023-03-24 21:53:05 +0800

    add

--- bin/start.ts
+++ bin/start.ts
@@ -1,22 +1,79 @@
-import { Command } from "commander";
 import logger from "../src/utils/logger";
 import App from "../src/app";
+const yargs = require("yargs");
 
+process.on("uncaughtException", (e) => console.error("uncaughtException ", e.stack));
+process.on("unhandledRejection", (e: any) => logger.warn("unhandledRejection", e?.stack || ""));
 
-const program = new Command();
-let appParams: any = program //
-   .option("-p, --port [value]", "绑定端口", "80") //
-   .option("-h, --host [value]", "绑定IP", "0.0.0.0") //
-   .parse(process.argv)
-   .opts();
+const optimistUsageLength = 98;
 
-console.info("====>>>start", appParams);
-logger.info("test===", "ok", "xx");
-logger.user.info("user print", "aaa");
+const opts = yargs
+   .usage("Usage: $0")
+   .wrap(Math.min(optimistUsageLength, yargs.terminalWidth()))
+   .options({
+      expire_timeout: {
+         demandOption: false,
+         alias: "t",
+         describe: "timeout (milliseconds)",
+         default: 5000,
+      },
+      concurrent_limit: {
+         demandOption: false,
+         alias: "c",
+         describe: "concurrent limit",
+         default: 5000,
+      },
+      alive_timeout: {
+         demandOption: false,
+         describe: "broken connection check timeout (milliseconds)",
+         default: 60000,
+      },
+      key: {
+         demandOption: false,
+         alias: "k",
+         describe: "connection key",
+         default: "peerjs",
+      },
+      sslkey: {
+         demandOption: false,
+         describe: "path to SSL key",
+      },
+      sslcert: {
+         demandOption: false,
+         describe: "path to SSL certificate",
+      },
+      host: {
+         demandOption: false,
+         alias: "H",
+         describe: "host",
+         default: "0.0.0.0",
+      },
+      port: {
+         demandOption: true,
+         alias: "p",
+         describe: "port",
+         default: 80,
+      },
+      path: {
+         demandOption: false,
+         describe: "custom path",
+         default: "/",
+      },
+      allow_discovery: {
+         demandOption: false,
+         describe: "allow discovery of peers",
+      },
+      proxied: {
+         demandOption: false,
+         describe: "Set true if PeerServer stays behind a reverse proxy",
+         default: false,
+      },
+   })
+   .boolean("allow_discovery").argv;
 
 (async () => {
    const app = new App();
-   let server = app.listen(appParams.port, appParams.host, () => {
+   let server = app.listen(opts.port, opts.host, () => {
       let address: any = server.address();
       console.info("server listen ", address);
    });
